831c9dfbbdda03f5db60256d185f299804a30554
Ege <52561965+GianC-Dev@users.noreply.github.com>
2020-05-14 14:45:16 +0300

    v1 - first work

--- Nitro.py
+++ Nitro.py
@@ -1,9 +1,6 @@
 import random
 import string
-
-from selenium import webdriver
-import warnings
-
+import requests
 
 
 def gencode():
@@ -11,22 +8,24 @@ def gencode():
 	return ''.join(random.choice(letters) for i in range(19))
 
 
-def check():
-	warnings.filterwarnings('ignore')
-	brwsr = webdriver.PhantomJS()
-	while True:
-		code = gencode()
-		brwsr.get(
-			"https://discordapp.com/api/v6/entitlements/gift-codes/" + code + "?with_application=false&with_subscription_plan=true")
-		comedtext = brwsr.find_element_by_xpath('/html/body/pre').text
-		if comedtext[36:-29] == 'You are being rate limited.':
-			print("Not worked: " + code)
-		else:
-			print("Worked: " + code)
-			file = open("workedcodes.txt", "a+")
-			file.write("\n"+code)
+class NitroGenerator:
+	def __init__(self):
+		self.codes = []
+		self.check()
+	
+	def check(self):
+		while True:
+			code = gencode()
+			self.codes.append(code)
+			response = requests.get(
+				"https://discordapp.com/api/v6/entitlements/gift-codes/" + code + "?with_application=false&with_subscription_plan=true")
+			data = response.json()
+			if data["message"] == 'You are being rate limited.':
+				print("Not worked: " + code)
+			else:
+				print("Worked: " + code)
+				file = open("workedcodes.txt", "a+")
+				file.write("\n" + code)
 
 
-# https://discordapp.com/api/v6/entitlements/gift-codes/CODEE?with_application=false&with_subscription_plan=true
-
-check()
+NitroGenerator()
