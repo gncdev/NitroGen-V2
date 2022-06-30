29a5d5eca72ba447f788048f9c28c29ddc26d8ba
T3g4n <68778866+TT3g4n@users.noreply.github.com>
2022-06-30 16:32:53 +0100

    Update it for more possibilities with the API, and wait for the specified time while rate limited.

--- Nitro.py
+++ Nitro.py
@@ -1,35 +1,37 @@
 import random
 import string
+from time import sleep
 import requests
 
 
 def gencode():
-	letters = string.ascii_letters + string.digits
-	return ''.join(random.choice(letters) for i in range(19))
+    letters = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters) for i in range(19))
 
 
 class NitroGenerator:
-	def __init__(self):
-		self.codes = []
-		self.check()
-	
-	def check(self):
-		while True:
-			code = gencode()
-			self.codes.append(code)
-			response = requests.get(
-				"https://discord.com/api/v7/entitlements/gift-codes/" + code + "?with_application=false&with_subscription_plan=true")
-			data = response.json()
-			if data["message"] == 'Unknown Gift Code':
-				print("Not worked: " + code)
-                        elif data["message"] == 'You are being rate limited.':
-                                print('Rate Limited: ' + code)
-                                file = open("ratelimited.txt", "a+")
-                                file.write("\n" + code)
-			else:
-				print("Worked: " + code)
-				file = open("workedcodes.txt", "a+")
-				file.write("\n" + code)
+    def __init__(self):
+        self.codes = []
+        self.check()
+
+    def check(self):
+        while True:
+            code = gencode()
+            self.codes.append(code)
+            response = requests.get(
+                "https://discord.com/api/v7/entitlements/gift-codes/" + code + "?with_application=false&with_subscription_plan=true")
+            data = response.json()
+            if data["message"] == 'Unknown Gift Code':
+                print("Not worked: " + code)
+            elif data["message"] == 'The resource is being rate limited.' or 'You are being rate limited.':
+                print('Rate Limited: ' + code)
+                print('Waiting')
+                print(data)
+                sleep(int(data['retry_after'])/1000)
+            else:
+                print("Worked: " + code)
+                file = open("workedcodes.txt", "a+")
+                file.write("\n" + code)
 
 
 NitroGenerator()
